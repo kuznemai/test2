1281e0fbed6b991263dd3dee3defd107e1984842
Marina <marinakuznetsova420@gmail.com>
2025-10-03 15:25:55 +0300

    breadcrumb paths had been tested

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,41 +1,42 @@
 <script setup lang="ts">
 import type { BreadcrumbItem } from "@nuxt/ui";
 
-const options = ["option1", "option2", "option3", "option4"];
+const options = ["Белый", "Рыжий", "Серый", "Чёрный"];
 const items = ref<BreadcrumbItem[]>([
-  {
-    label: "Docs",
-    icon: "i-lucide-book-open",
-    to: "/docs",
-  },
-  {
-    label: "Components",
-    icon: "i-lucide-box",
-    to: "/docs/components",
-  },
-  {
-    label: "Breadcrumb",
-    icon: "i-lucide-link",
-    to: "/docs/components/breadcrumb",
-  },
+  { label: "Магазин", icon: "i-lucide-store", to: "/shop" },
+  { label: "Коты", icon: "i-lucide-cat", to: "/shop/cats" },
+  { label: "Кот №1", icon: "i-lucide-paw" },
 ]);
-const value = ref(options[0]);
+const value = ref(options[1]);
+function buyCat() {
+  alert(`Вы купили кота цвета: ${value.value} `);
+}
 </script>
 
 <template>
   <section class="p-8 space-y-8">
     <UBreadcrumb :items="items" />
 
-    <div id="demo" class="space-y-4">
-      <h1 class="text-3xl font-bold">Nuxt UI speed test</h1>
+    <h1 class="text-3xl font-bold">Купить кота</h1>
 
-      <div class="flex items-center gap-4">
-        <USelect v-model="value" :items="options" class="w-60" />
-        <UButton label="Нажми меня" @click="console.log('clicked')" />
-      </div>
-    </div>
-    <div>
-      <CatImage></CatImage>
+    <div class="grid md:grid-cols-2 gap-8 items-start">
+      <CatImage />
+
+      <UCard class="space-y-6">
+        <div class="space-y-4">
+          <UFormGroup label="Выберите цвет кота">
+            <USelect v-model="value" :items="options" class="w-60 m-3" />
+          </UFormGroup>
+
+          <UButton
+            icon="i-lucide-shopping-cart"
+            color="primary"
+            @click="buyCat"
+          >
+            Купить кота
+          </UButton>
+        </div>
+      </UCard>
     </div>
   </section>
 </template>

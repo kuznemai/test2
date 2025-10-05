62545afb22074dcd543f8c25f5a9997384c9b593
Marina <marinakuznetsova420@gmail.com>
2025-10-05 20:19:28 +0300

    добавлен ssr

--- app/pages/shop/index.vue
+++ app/pages/shop/index.vue
@@ -1,6 +1,4 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <section class="space-y-6">
@@ -8,7 +6,10 @@
 
     <UCard>
       <h2 class="text-xl font-bold">Магазин котов</h2>
-      <p class="mt-2">Итак, вы решились на покупку пушистого друга. Наш каталог котов точно не оставит Вас равнодушным и без кота Вы не уйдете :)</у></p>
+      <p class="mt-2">
+        Итак, вы решились на покупку пушистого друга. Наш каталог котов точно не
+        оставит Вас равнодушным и без кота Вы не уйдете :)
+      </p>
       <NuxtLink to="/shop/cats" class="block mt-4 text-primary hover:underline">
         Перейти к списку котов →
       </NuxtLink>
